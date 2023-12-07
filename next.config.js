2360bf6c422fe9da99628210e436ac3b8e6f75a6
faisallyt <96649676+faisallyt@users.noreply.github.com>
2023-12-07 20:47:35 +0530

    commited

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+//  @type {import('next').NextConfig} 
+const nextConfig = {
+    images:{
+        domains:[
+            "oaidalleapiprodscus.blob.core.windows.net",
+        ]
+    }
+}
 
 module.exports = nextConfig
